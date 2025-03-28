b3da0e9a2088c4f7117f38385cacc512952a00cf
NicholasAngel999 <crazyblader05@gmail.com>
2025-03-28 16:17:24 +0000

    going for a walk

--- Python/quiz.py
+++ Python/quiz.py
@@ -8,7 +8,14 @@ print('''FYI! when writing your answers please keep everything lower case and do
 
 print(' Are you ready!')
 
+import time
+
 score = 0
+
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(i, end="...\n", flush=True)
+        time.sleep(1)
 
 while True:
     start = input("type 'yes' to continue: ").strip().lower()
@@ -19,6 +26,7 @@ while True:
         print("Try Again.")
 
 while True:
+    countdown(3)
     Question_1 = input("what was my favourite Holiday? ").strip().lower()
     if Question_1 == "florida":
         print("Yes! 1 point!")
@@ -27,8 +35,10 @@ while True:
     else:
         print("Oooh no! It was Florida! No points")
         break # Exit the loop go to question_2
+    
 
 while True:
+    countdown(3)
     question_2 = input("what was my favourite park at Disney World in Florida? ").strip().lower()
     if question_2 == "hollywood studios":
         print("Great Job! 1 Point!")
@@ -38,7 +48,9 @@ while True:
         print("Good Guess! No Points")
         break
 
+t
 while True:
+    countdown(3)
     question_3 = input("Which of our 3 visits to the USA was my favourite? Please answer either 1 2 or 3 ").strip().lower()
     if question_3 == "1":
         print("Yess! It was a great first experience 1 Point!")
@@ -48,7 +60,9 @@ while True:
         print("It could have been any of them it was hard to decide. 0 Points")
         break
 
+
 while True:
+    countdown(3)
     question_4 = input("What was my Favourite ride throughout all of the Disney Parks in Florida? ").strip().lower()
     if question_4 == "tower of terror" or "the tower of terror":
         print("It was a great ride! Especially with Kirsty. 1 Point!")
@@ -58,7 +72,9 @@ while True:
         print("close but not quite. 0 Points")
         break
 
+
 while True:
+    countdown(3)
     question_5 = input("which was my favourite universal studios park? florida or california? ").strip().lower()
     if question_5 == "florida":
         print("It was close but still florida")
@@ -68,7 +84,9 @@ while True:
         print("you had a 50/50!")
         break
 
+
 while True:
+    countdown(3)
     q_6 = input("look at image help yeti, and tell me what ride we were in the queue for when we saw this? ").strip().lower()
     if q_6 == "expedition everest":
         print("amazing memory")
@@ -79,6 +97,7 @@ while True:
         break
 
 while True:
+    countdown(3)
     q_7 = input("do you remember which park that ride was from? ").strip().lower()
     if q_7 == "animal kingdom":
         print("tough one")
@@ -86,8 +105,11 @@ while True:
         break
     else:
         print("it could have been loads")
+        break
+
 
 while True:
+    countdown(3)
     q_8 = input("Which of the four swimming pools was the best at the art of animation? ").strip().lower()
     if q_8 == "finding nemo":
         score += 1
@@ -96,15 +118,20 @@ while True:
         print("cars was a close second for the cones")
         break
 
+
 while True:
+    countdown(3)
     q_9 = input("on the 27th of october we went to the chocolate emporium. What milkshake did i get and which did mum get? ").strip().lower()
     if q_9 == "chocolate brownie red velvet" or "chocolate brownie and red velvet":
         score += 1 
         break
     else:
         print("even i may be wrong here")
+        break
+
 
 while True:
+    countdown(3)
     q_10 = input("before the chocolate emporium we were in universal what ride caused mum to get a new shirt? just use character name. ").strip().lower()
     if q_10 == "popeye":
         score += 1
@@ -113,7 +140,9 @@ while True:
         print("how do you forget that!")
         break
 
+
 while True:
+    countdown(3)
     q_11 = input("what did mum do to annoy one of the toy soldiers whilst trying to take pictures? ").strip().lower()
     if q_11 == "stand on his feet":
         print("so many times")
@@ -123,13 +152,29 @@ while True:
         print("that felt like an easy one")
         break
 
+
 while True:
+    countdown(3)
     q_12 = input("look at the attached q 12 help file. Where was this picture taken? ").strip().lower()
-    if q_12 == "Durham":
+    if q_12 == "durham":
         print("yes! do you remember the bus questions?")
         score += 1
         break
     else:
         print("How do you forget?")
+        break
+
+while True:
+    countdown(3)
+    q_13 = input("whos harry potter room was in here? ").strip().lower()
+    if q_13 == "mcgonagall":
+        print("easy!")
+        score += 1
+        break
+    else:
+        print("i basically gave you a point!")
+        break
+
+
 
 
