fa8d4c3add3d79809f3d8f68edb173a35d3c0b91
NicholasAngel999 <crazyblader05@gmail.com>
2025-04-01 13:31:09 +0100

    quiz start amy

--- Python/amy quiz.py
+++ Python/amy quiz.py
@@ -5,3 +5,18 @@
 #sneak some climbing in there 
 #maybe some funny questions about me 
 
+import time
+import sys
+
+def type_text(text, delay=0.05):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+score = 0
+
+type_text("Hey baby, ive made you a quiz about all sorts of things from films and makeup to me and surfing")
+
+
