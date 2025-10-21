fa9d2797e0c0d71af2cbcb805538200901da3bb8
NicholasAngel999 <colesumner05@gmail.com>
2025-10-21 16:40:27 +0100

    ahhhh

---
+++ Python/Password_manager/Password_manager_gui.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+import getpass
+
+import Password_manager as pm
+
+root = tk.Tk()
+root.title("Password Manager")
+root.geometry("400x300")
+root.resizable(False, False)
+
+# ------------------------------
+# GUI
+
