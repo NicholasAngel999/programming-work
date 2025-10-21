cd8ab052e741cfd42fa47992423611406b9a3a8b
NicholasAngel999 <colesumner05@gmail.com>
2025-10-21 16:56:11 +0100

    gui add password??

--- Python/Password_manager/Password_manager_gui.py
+++ Python/Password_manager/Password_manager_gui.py
@@ -13,3 +13,16 @@ root.mainloop()
 # ------------------------------
 # GUI
 
+def gui_add_password():
+    website = entry_website.get().strip()
+    password = entry_password.get().strip()
+    if not website or not password:
+        messagebox.showwarning("missing Info, Please fill in both fields.")
+        return
+    pm.add_password(website, password)
+    messagebox.showinfo("Success", f"Password saved for {website}!")
+    entry_website.delete(0, tk.EMD)
+    entry_password.delete(0, tk.END)
+
+    
+
