2a03cc8b82d3f6665a9a1a7bf863e9badfef21f3
NicholasAngel999 <colesumner05@gmail.com>
2026-02-18 02:23:39 +0000

    things have been made

--- Python/Chess_game/chess.py
+++ Python/Chess_game/chess.py
@@ -6,12 +6,39 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 900
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Chess Pygame!')
 font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
 
 # game variables and images
+white_pieces = ['rook', 'kmight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+
+
+black_pieces = ['rook', 'kmight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn']
+
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7)
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
+
+captured_pieces_white = []
+captured_pieces_black = []
+
+# 0 - white turn, no selection: 1-whites turn, place selected: 2 - black turn, no selected: 3 - black turn peice selected
+turn_step = 0
+selection = 100
+valid_moves = []
+# load in game piece images (queen king rook bishop knight pawn) x 2
+# piece_variable = pygame.image.load('path/to/image.png')
+
+
+
 
 # main game loop
 
