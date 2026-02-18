c6ef5a488dc50ceb8098c7818481f9b3e213e4cd
NicholasAngel999 <colesumner05@gmail.com>
2026-02-18 12:21:32 +0000

    images lists updated for small pieces

--- Python/Chess_game/chess.py
+++ Python/Chess_game/chess.py
@@ -88,6 +88,9 @@ white_queen_small = pygame.transform.scale(white_queen, (45, 45))
 white_images = [white_pawn, white_rook, white_knight, white_bishop, white_queen, white_king]
 black_images = [black_pawn, black_rook, black_knight, black_bishop, black_queen, black_king]
 
+white_images_small = [white_pawn_small, white_rook_small, white_knight_small, white_bishop_small, white_queen_small, white_king_small]
+black_images_small = [black_pawn_small, black_rook_small, black_knight_small, black_bishop_small, black_queen_small, black_king_small]
+
 
 # main game loop
 
