2ef613b8dee8a488e05ba46a84dd211aa15caa19
NIcholasAngel999 <colesumner05@gmail.com>
2026-04-08 15:17:12 +0100

    vs code made nicer still split between this and nvim

--- Python/Chess_game/chess.py
+++ Python/Chess_game/chess.py
@@ -216,13 +216,13 @@ def check_valid_moves():
 
 
 # draw valid moves on screen
- def draw_valid(moves):
-     if turn_step < 2:
+def draw_valid(moves):
+    if turn_step < 2:
         color = 'red'
     else:
         color = 'blue'
     for i in range(len(moves)):
-    pygame.draw.circle(screen, color, (moves[i][0] * 100 + 50, moves[i][1] * 100 +50), 5)
+        pygame.draw.circle(screen, color, (moves[i][0] * 100 + 50, moves[i][1] * 100 + 50), 5)
 
 
 
